83d8d24bc44fda5b9760b4916685c75b9b259cf0
OccRx <ilippolitov@gmail.com>
2022-01-23 16:50:37 +0300

    v1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,18 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat bean1 =
+                (Cat) applicationContext.getBean("cat");
+        Cat bean11 =
+                (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean == bean2);
+        System.out.println(bean1 == bean11);
     }
 }
